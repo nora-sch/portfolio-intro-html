d2df68d5f7bfcb530778b3dc681560ff5518c720
nora-sch <nora.schumann.development@gmail.com>
2021-09-30 09:14:52 +0200

    add under construction sign

---
+++ home/js/dist/home.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var underConstuction = document.getElementById("under-construction");
+underConstuction.addEventListener("click", function (event) {
+  event.preventDefault();
+  underConstuction.classList.add("hidden");
+});
---
+++ home/js/home.js
@@ -0,0 +1,5 @@
+let underConstuction = document.getElementById("under-construction");
+underConstuction.addEventListener("click", function (event) {
+  event.preventDefault();
+  underConstuction.classList.add("hidden");
+});
